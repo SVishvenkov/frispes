4646ba0349fa16a6b39899488feb91dae7489deb
SVishvenkov <svishvenkov@mail.ru>
2022-04-28 13:49:59 +0300

    fin

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,28 @@
-console.log('script work!');
+$(document).ready(function () {
+
+    $('.j-gallery-card').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+});
